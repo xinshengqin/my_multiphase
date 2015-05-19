fe91016c9bef25f5e0db80d136ac5d45ec445975
xinshengqin <xinshengqin2014@gmail.com>
2015-05-18 22:02:10 -0700

    finished testing u_star v_star

--- projection_method.py
+++ projection_method.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python
 from mesh import Mesh_1d,Mesh_2d
 from fields import Fields_2d 
-from fields_2d_velocity import U,V
 from boundary_conditions import *
+from fields_2d_velocity import U,V
+from initial_conditions import *
+import numpy as np
+
 
 
 #create 2d mesh
-meshx = Mesh_1d(0.,10.,0.1,0.1,'mesh_x')
-meshy = Mesh_1d(0.,4.,0.1,0.1,'mesh_y')
-#meshx.plot_edgeVsIndex()
-#meshx.plot_centerVsIndex()
-#meshy.plot_edgeVsIndex()
-#meshy.plot_centerVsIndex()
+meshx = Mesh_1d(0.,4.,2,2,'mesh_x')
+meshy = Mesh_1d(0.,2,0.5,0.5,'mesh_y')
+meshx.plot_edgeVsIndex()
+meshx.plot_centerVsIndex()
+meshy.plot_edgeVsIndex()
+meshy.plot_centerVsIndex()
 mesh = Mesh_2d(meshx,meshy,'mesh')
 print "Mesh created.\n"
 print "Mesh size: (",mesh.nx,', ',mesh.ny,")"
@@ -22,18 +25,23 @@ u_inf = 1
 max_iterations = 10000
 #todo defined a IC function for this problem
 #todo modify BC
-u = U(mesh,BC_fixedValue_1,IC)
-v = V(mesh,BC_fixedValue_1,IC)
-p = Fields_2d(mesh,BC_fixedValue_1,IC)
+u = U(mesh,BC_empty,IC_test_interpolation_u)
+v = V(mesh,BC_empty,IC_test_interpolation_v)
+p = Fields_2d(mesh,BC_fixedValue_1,IC='default')
 fields_list = [u,v,p]
 
 n_steps=10000 #Interval that u,v and p are write
 Re=100.0   #Reynolds number
 #nu=1e-6
-mu = 1e-3
-rho=1000.0 
+mu = 1e-2
+rho=1.0 
 t=0.0
-dt=CFL*mesh.min_delta/u_inf
+dt=CFL*meshx.min_delta/u_inf/Re
+
+#write all fields into time directory
+for i,item in enumerate(fields_list):
+    item.write(str(t))
+
 
 #write all fields into time directory
 for i,item in enumerate(fields_list):
@@ -52,17 +60,18 @@ for n in range(max_iterations):
 
 
     # Step 1: Update velocity to intermediate step
-    # Calculate fluxes at each boundary
     Ax = u.uphi_x_vedge(u)+v.vphi_y_vedge(u)
     Dx = mu*(u.ddx2()+u.ddy2())
     Ay = u.uphi_x_hedge(v)+v.vphi_y_hedge(v)
     Dy = mu*(v.ddx2()+v.ddy2())
-    #todo
     fbx = Fields_2d(mesh,ftype='vedge')#currently its zero
     fby = Fields_2d(mesh,ftype='hedge')#currently its zero
             
-    u_star.value = u.value + dt*(-1.*Ax+fbx+1./rho*(Dx))
-    v_star.value = v.value + dt*(-1.*Ay+fby+1./rho*(Dy))
+    u_star = u + (dt*(-1.*Ax+fbx+1./rho*(Dx)))
+    v_star = v + (dt*(-1.*Ay+fby+1./rho*(Dy)))
+
+    #step 2: 
+
 
     break
     
